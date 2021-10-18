f47ca97f5232cb8fd84fa7183b494d4eb5b2b78b
SisoroT <rtaylor6410@gmail.com>
2021-10-18 15:41:30 -0400

    new autopairs plugin

---
+++ plugins/autopairs.lua
@@ -0,0 +1,16 @@
+require('nvim-autopairs').setup{
+  -- enable_check_bracket_line = true,
+
+  -- change default fast_wrap
+  fast_wrap = {
+    map = '<S-w>',
+    chars = { '{', '[', '(', '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+    offset = 0, -- Offset from pattern match
+    end_key = 'w',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    hightlight = 'Search'
+  },
+
+}
