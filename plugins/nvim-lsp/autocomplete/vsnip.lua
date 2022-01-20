e12f4f07aba95abce29c2cc23f425194815df6a1
SisoroT <rtaylor6410@gmail.com>
2022-01-19 20:14:05 -0500

    formatted the lua code

--- plugins/nvim-lsp/autocomplete/vsnip.lua
+++ plugins/nvim-lsp/autocomplete/vsnip.lua
@@ -2,14 +2,14 @@ local g = vim.g
 local cmd = vim.cmd
 
 g.vsnip_filetypes = {
-  javascriptreact = { 'javascript' },
-  typescriptreact = { 'typescript' },
-  javareact = { 'java' },
+	javascriptreact = { "javascript" },
+	typescriptreact = { "typescript" },
+	javareact = { "java" },
 }
 
-cmd [[
+cmd([[
   imap <expr> <C-j> vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<C-j>'
   smap <expr> <C-j> vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<C-j>'
   imap <expr> <C-k> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<C-k>'
   smap <expr> <C-k> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<C-k>'
-]]
+]])
