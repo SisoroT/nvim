bfe69bff272bf83ca94230ee4b1cb8a45d08ad4c
SisoroT <rtaylor6410@gmail.com>
2022-01-19 20:16:51 -0500

    formatted the lua code

--- plugins/autopairs.lua
+++ plugins/autopairs.lua
@@ -1,16 +1,15 @@
-require('nvim-autopairs').setup{
-  -- enable_check_bracket_line = true,
+require("nvim-autopairs").setup({
+	-- enable_check_bracket_line = true,
 
-  -- change default fast_wrap
-  fast_wrap = {
-    map = '<C-e>',
-    chars = { '{', '[', '(', '"', "'" },
-    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
-    offset = 0, -- Offset from pattern match
-    end_key = 'w',
-    keys = 'qwertyuiopzxcvbnmasdfghjkl',
-    check_comma = true,
-    hightlight = 'Search'
-  },
-
-}
+	-- change default fast_wrap
+	fast_wrap = {
+		map = "<C-e>",
+		chars = { "{", "[", "(", '"', "'" },
+		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+		offset = 0, -- Offset from pattern match
+		end_key = "w",
+		keys = "qwertyuiopzxcvbnmasdfghjkl",
+		check_comma = true,
+		hightlight = "Search",
+	},
+})
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,31 +1,30 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-  },
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	highlight = {
+		enable = true, -- false will disable the whole extension
+	},
 
-  indent = {
-      enable = false,
-  },
+	indent = {
+		enable = false,
+	},
 
-  rainbow = {
-      enable = true,
-      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-      max_file_lines = nil, -- Do not enable for files with more than n lines, int
-      colors = {
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		colors = {
 
-        "#ff5555",
-        "#ffb86c",
-        "#f1fa8c",
-        "#50fa7b",
-        "#8be9fd",
-        "#ff79c6",
-        "#bd93f9"
-      }, -- table of hex strings
-      -- termcolors = {} -- table of colour name strings
-  },
-
-}
+			"#ff5555",
+			"#ffb86c",
+			"#f1fa8c",
+			"#50fa7b",
+			"#8be9fd",
+			"#ff79c6",
+			"#bd93f9",
+		}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+})
 
 -- allows bracket rainbow parens plugin to still come through
 -- require"nvim-treesitter.highlight"
