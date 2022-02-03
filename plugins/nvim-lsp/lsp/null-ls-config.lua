bd7d21714d9e6237601bfc41596a2c26c750cdf5
SisoroT <rtaylor6410@gmail.com>
2022-02-03 16:15:32 -0500

    added spellchecker

--- plugins/nvim-lsp/lsp/null-ls-config.lua
+++ plugins/nvim-lsp/lsp/null-ls-config.lua
@@ -1,6 +1,5 @@
 require("null-ls").setup({
 	sources = {
-		require("null-ls").builtins.diagnostics.flake8,
 		require("null-ls").builtins.formatting.black.with({
 			extra_args = { "--fast" },
 		}),
@@ -8,7 +7,9 @@ require("null-ls").setup({
 		-- 	extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
 		-- }),
 		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.completion.spell,
+		require("null-ls").builtins.formatting.codespell,
+		require("null-ls").builtins.diagnostics.codespell,
+		require("null-ls").builtins.diagnostics.flake8,
 	},
 
 	on_attach = function(client)
