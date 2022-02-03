3654fa3d32980c4e82f05f71ab99afda5df87767
SisoroT <rtaylor6410@gmail.com>
2022-02-03 15:06:36 -0500

    some highlighting changes

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,11 +1,16 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	sync_install = false,
 	highlight = {
 		enable = true, -- false will disable the whole extension
+		additional_vim_regex_highlighting = false,
 	},
 
 	indent = {
 		enable = false,
+	},
+	autopairs = {
+		enable = true,
 	},
 
 	rainbow = {
