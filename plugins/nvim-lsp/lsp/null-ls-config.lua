084e5a49cff80b57acfd58657cd79fe000c17544
SisoroT <rtaylor6410@gmail.com>
2022-05-09 21:13:22 -0400

    format on save fixed, prettier fixed

--- plugins/nvim-lsp/lsp/null-ls-config.lua
+++ plugins/nvim-lsp/lsp/null-ls-config.lua
@@ -1,23 +1,23 @@
 require("null-ls").setup({
-	sources = {
-		require("null-ls").builtins.formatting.black,
-		-- require("null-ls").builtins.formatting.prettier.with({
-		-- 	extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-		-- }),
-		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.formatting.codespell,
-		require("null-ls").builtins.diagnostics.codespell,
-		require("null-ls").builtins.diagnostics.flake8,
-	},
+    sources = {
+        require("null-ls").builtins.formatting.black,
+        require("null-ls").builtins.formatting.prettier.with({
+            extra_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
+        }),
+        require("null-ls").builtins.formatting.stylua,
+        -- require("null-ls").builtins.formatting.codespell,
+        require("null-ls").builtins.diagnostics.codespell,
+        require("null-ls").builtins.diagnostics.flake8,
+    },
 
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd([[
+    on_attach = function(client)
+        if client.server_capabilities.documentFormattingProvider then
+            vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
             augroup END
             ]])
-		end
-	end,
+        end
+    end,
 })
