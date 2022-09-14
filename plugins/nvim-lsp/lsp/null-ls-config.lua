d1569f07e9ea679666c1624efc1021257a9bb477
SisoroT <rtaylor6410@gmail.com>
2022-09-14 13:20:03 -0400

    added diagnostics and code actions for js

--- plugins/nvim-lsp/lsp/null-ls-config.lua
+++ plugins/nvim-lsp/lsp/null-ls-config.lua
@@ -1,36 +1,38 @@
 require("null-ls").setup({
-    sources = {
-        -- python
-        require("null-ls").builtins.formatting.black,
-        require("null-ls").builtins.diagnostics.flake8,
+	sources = {
+		-- python
+		require("null-ls").builtins.formatting.black,
+		require("null-ls").builtins.diagnostics.flake8,
 
-        -- lua
-        require("null-ls").builtins.formatting.stylua,
+		-- lua
+		require("null-ls").builtins.formatting.stylua,
 
-        -- javascirpt, css, html
-        require("null-ls").builtins.formatting.prettier.with({
-            extra_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
-        }),
+		-- javascirpt, css, html
+		require("null-ls").builtins.formatting.prettier.with({
+			extra_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		require("null-ls").builtins.diagnostics.eslint,
+		require("null-ls").builtins.code_actions.eslint,
 
-        -- sql
-        require("null-ls").builtins.formatting.pg_format,
-        require("null-ls").builtins.diagnostics.sqlfluff.with({
-            extra_args = { "--dialect", "mysql" },
-        }),
+		-- sql
+		require("null-ls").builtins.formatting.pg_format,
+		require("null-ls").builtins.diagnostics.sqlfluff.with({
+			extra_args = { "--dialect", "mysql" },
+		}),
 
-        -- spellchecking
-        -- require("null-ls").builtins.formatting.codespell,
-        require("null-ls").builtins.diagnostics.codespell,
-    },
+		-- spellchecking
+		-- require("null-ls").builtins.formatting.codespell,
+		require("null-ls").builtins.diagnostics.codespell,
+	},
 
-    on_attach = function(client)
-        if client.server_capabilities.documentFormattingProvider then
-            vim.cmd([[
+	on_attach = function(client)
+		if client.server_capabilities.documentFormattingProvider then
+			vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
                 autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
             augroup END
             ]])
-        end
-    end,
+		end
+	end,
 })
