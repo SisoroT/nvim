1cbf308d927f73e2e48cbed950379473866e690f
SisoroT <rtaylor6410@gmail.com>
2022-09-14 13:22:30 -0400

    switched from vsnip to luasnip

--- plugins/nvim-lsp/autocomplete/cmp-config.lua
+++ plugins/nvim-lsp/autocomplete/cmp-config.lua
@@ -2,57 +2,57 @@ local cmp = require("cmp")
 local lspkind = require("lspkind")
 
 cmp.setup({
-    completion = {
-        completeopt = "menuone,noinsert",
-    },
-    snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-        end,
-    },
-    mapping = {
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        ["<Tab>"] = cmp.mapping.select_next_item(),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-        }),
-    },
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.kind = lspkind.presets.default[vim_item.kind]
-            return vim_item
-        end,
-    },
-    experimental = {
-        ghost_text = true,
-    },
-    -- You should specify your *installed* sources.
-    sources = {
-        { name = "buffer" },
-        { name = "path" },
-        { name = "vsnip" },
-        { name = "calc" },
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "vim-dadbod-completion" },
-    },
+	completion = {
+		completeopt = "menuone,noinsert",
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	mapping = {
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<M-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		}),
+	},
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = lspkind.presets.default[vim_item.kind]
+			return vim_item
+		end,
+	},
+	experimental = {
+		ghost_text = true,
+	},
+	-- You should specify your *installed* sources.
+	sources = {
+		{ name = "luasnip" },
+		{ name = "vim-dadbod-completion" },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "calc" },
+	},
 })
 
 -- you need setup cmp first put this after cmp.setup()
 require("cmp").setup({
-    map_cr = true, --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-    auto_select = true, -- automatically select the first item
-    insert = false, -- use insert confirm behavior instead of replace
-    map_char = { -- modifies the function or method delimiter by filetypes
-        all = "(",
-        tex = "{",
-    },
+	map_cr = true, --  map <CR> on insert mode
+	map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
+	auto_select = true, -- automatically select the first item
+	insert = false, -- use insert confirm behavior instead of replace
+	map_char = { -- modifies the function or method delimiter by filetypes
+		all = "(",
+		tex = "{",
+	},
 })
 
 vim.api.nvim_command("autocmd VimEnter * hi CmpItemAbbrMatch guifg=white")
