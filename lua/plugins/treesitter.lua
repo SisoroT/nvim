f6cb1818d966a946a5bc442501be8f18ca9dd837
SisoroT <rtaylor6410@gmail.com>
2023-03-07 11:26:01 -0500

    fixed broken functionality by removing "comment" parser and added comments to better explain things

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,26 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	-- A list of parser names, or "all" (the listed parsers should always be installed)
+	ensure_installed = "all",
+	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
-
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+	-- List of parsers to ignore installing (for "all")
+	ignore_install = { "comment" },
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		additional_vim_regex_highlighting = false,
 	},
-
 	indent = {
 		enable = false,
 	},
-
 	autopairs = {
 		enable = true,
 	},
 	autotag = {
 		enable = true,
 	},
-
 	rainbow = {
 		enable = true,
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
