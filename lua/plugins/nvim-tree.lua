91e27f4a99c9e0688684fc0b52c3448e0af93d83
SisoroT <rtaylor6410@gmail.com>
2023-04-19 15:27:24 -0400

    way to do keybinds changed

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,108 +1,70 @@
--- following options are the default
+-- keybinds
+local function on_attach(bufnr)
+	local map = vim.keymap.set
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	map("n", "o", api.node.open.edit, opts("Open"))
+	map("n", "O", api.node.open.no_window_picker, opts("Open: No Window Picker"))
+	map("n", "<CR>", api.tree.change_root_to_node, opts("CD"))
+	map("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+	map("n", "<C-x>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+	map("n", "<C-t>", api.node.open.tab, opts("Open: New Tab"))
+	map("n", "<", api.node.navigate.sibling.prev, opts("Previous Sibling"))
+	map("n", ">", api.node.navigate.sibling.next, opts("Next Sibling"))
+	map("n", "P", api.node.navigate.parent, opts("Parent Directory"))
+	map("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	map("n", "l", api.node.open.edit, opts("Open"))
+	map("n", "<Tab>", api.node.open.preview, opts("Open Preview"))
+	map("n", "K", api.node.navigate.sibling.first, opts("First Sibling"))
+	map("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
+	map("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
+	map("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+	map("n", "R", api.tree.reload, opts("Refresh"))
+	map("n", "a", api.fs.create, opts("Create"))
+	map("n", "d", api.fs.remove, opts("Delete"))
+	map("n", "r", api.fs.rename, opts("Rename"))
+	map("n", "<C-r>", api.fs.rename_sub, opts("Rename: Omit Filename"))
+	map("n", "x", api.fs.cut, opts("Cut"))
+	map("n", "c", api.fs.copy.node, opts("Copy"))
+	map("n", "p", api.fs.paste, opts("Paste"))
+	map("n", "y", api.fs.copy.filename, opts("Copy Name"))
+	map("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+	map("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+	map("n", "[c", api.node.navigate.git.prev, opts("Prev Git"))
+	map("n", "]c", api.node.navigate.git.next, opts("Next Git"))
+	map("n", "<BS>", api.tree.change_root_to_parent, opts("Up"))
+	map("n", "s", api.node.run.system, opts("Run System"))
+	map("n", "q", api.tree.close, opts("Close"))
+	map("n", "g?", api.tree.toggle_help, opts("Help"))
+end
+
 require("nvim-tree").setup({
-	-- disables netrw completely
-	disable_netrw = false,
-	-- hijack netrw window on startup
-	hijack_netrw = true,
-	-- open the tree when running this setup function
-	open_on_setup = false,
-	-- will not open on setup if the filetype is in this list
-	ignore_ft_on_setup = {},
-	-- closes neovim automatically when the tree is the last **WINDOW** in the view
-	-- auto_close = true,
-	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-	open_on_tab = false,
-	-- hijacks new directory buffers when they are opened.
-	-- update_to_buf_dir = {
-	-- 	-- enable the feature
-	-- 	enable = true,
-	-- 	-- allow to open the tree if it was previously closed
-	-- 	auto_open = true,
-	-- },
+	on_attach = on_attach,
 	-- hijack the cursor in the tree to put it at the start of the filename
 	hijack_cursor = true,
-	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-	update_cwd = true,
 	-- show lsp diagnostics in the signcolumn
 	diagnostics = {
 		enable = true,
-		icons = {
-			hint = "",
-			info = "",
-			warning = "",
-			error = "",
-		},
 	},
 	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
 	update_focused_file = {
-		-- enables the feature
 		enable = true,
 		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
 		-- only relevant when `update_focused_file.enable` is true
 		update_cwd = true,
-		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-		ignore_list = {},
-	},
-	-- configuration options for the system open command (`s` in the tree by default)
-	system_open = {
-		-- the command to run this, leaving nil should work in most cases
-		cmd = nil,
-		-- the command arguments as a list
-		args = {},
 	},
 	view = {
 		-- width of the window, can be either a number (columns) or a string in `%`
 		width = 30,
-		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-		side = "left",
-		-- if true the tree will resize itself after opening a file
-		-- auto_resize = false,
-		mappings = {
-			-- custom only false will merge the list with the default mappings
-			-- if true, it will only use your list to set the mappings
-			custom_only = false,
-			-- list of mappings to set on the tree manually
-			list = {
-				{ key = { "o", "<2-LeftMouse>" }, action = "edit" },
-				{ key = { "<CR>" }, action = "cd" },
-				{ key = "<C-v>", action = "vsplit" },
-				{ key = "<C-x>", action = "split" },
-				{ key = "<C-t>", action = "tabnew" },
-				{ key = "<", action = "prev_sibling" },
-				{ key = ">", action = "next_sibling" },
-				{ key = "P", action = "parent_node" },
-				{ key = "h", action = "close_node" },
-				{ key = "l", action = "edit" },
-				{ key = "<Tab>", action = "preview" },
-				{ key = "K", action = "first_sibling" },
-				{ key = "J", action = "last_sibling" },
-				{ key = "I", action = "toggle_ignored" },
-				{ key = "H", action = "toggle_dotfiles" },
-				{ key = "R", action = "refresh" },
-				{ key = "a", action = "create" },
-				{ key = "d", action = "remove" },
-				{ key = "r", action = "rename" },
-				{ key = "<C-r>", action = "full_rename" },
-				{ key = "x", action = "cut" },
-				{ key = "c", action = "copy" },
-				{ key = "p", action = "paste" },
-				{ key = "y", action = "copy_name" },
-				{ key = "Y", action = "copy_path" },
-				{ key = "gy", action = "copy_absolute_path" },
-				{ key = "[c", action = "prev_git_item" },
-				{ key = "]c", action = "next_git_item" },
-				{ key = "<BS>", action = "dir_up" },
-				{ key = "s", action = "system_open" },
-				{ key = "q", action = "close" },
-				{ key = "g?", action = "toggle_help" },
-			},
-		},
 	},
 })
 
 -- launch commands
-vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", { noremap = true, silent = true })
 
 -- closes neovim automatically when the tree is the last **WINDOW** in the view
 vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
