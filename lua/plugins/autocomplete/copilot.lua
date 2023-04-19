886085346bc41da770bb642702ae8414675f6ad8
SisoroT <rtaylor6410@gmail.com>
2023-04-19 17:13:35 -0400

    updated format of keybinds

--- lua/plugins/autocomplete/copilot.lua
+++ lua/plugins/autocomplete/copilot.lua
@@ -1,14 +1,16 @@
--- options for keybinds
-local opts = { noremap = true, silent = true, script = true, expr = true }
+local function map(mode, keybind, command)
+	vim.keymap.set(mode, keybind, command, { noremap = true, silent = true, script = true, expr = true })
+end
 
 -- disable tab complete
 vim.g.copilot_no_tab_map = true
--- change keybind for accepting suggestions
-vim.api.nvim_set_keymap("i", "<M-a>", "copilot#Accept()", opts)
 
--- change keybind for next and previous suggestions
-vim.api.nvim_set_keymap("i", "<M-l>", "copilot#Next()", opts)
-vim.api.nvim_set_keymap("i", "<M-h>", "copilot#Previous()", opts)
+-- change keybind for accepting suggestions
+map("i", "<M-a>", "copilot#Accept()")
 
 -- change keybind to dismiss copilot suggestions
-vim.api.nvim_set_keymap("i", "<M-d>", "copilot#Dismiss()", opts)
+map("i", "<M-d>", "copilot#Dismiss()")
+
+-- change keybind for next and previous suggestions
+map("i", "<M-l>", "copilot#Next()")
+map("i", "<M-h>", "copilot#Previous()")
