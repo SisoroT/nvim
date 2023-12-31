4e5d5fe8efd7eec74269efa4cf12527444ff7594
SisoroT <rtaylor6410@gmail.com>
2023-12-30 21:45:34 -0500

    got rid of change detection and added keybind to bring up lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,24 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- open Lazy
+vim.keymap.set("n", "<leader>z", "<cmd>Lazy<cr>")
+
 local plugins = {
-    { import = "plugins" },
-    { import = "plugins.lsp" },
-    { import = "plugins.autocomplete" },
-    { import = "plugins.status-line" },
+	{ import = "plugins" },
+	{ import = "plugins.lsp" },
+	{ import = "plugins.autocomplete" },
+	{ import = "plugins.status-line" },
 }
 
 require("lazy").setup({
-    spec = plugins,
-    ui = { border = "rounded" },
+	spec = plugins,
+	ui = { border = "rounded" },
+	change_detection = { notify = false },
 })
