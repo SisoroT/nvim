3df90dda3b9cd3c6bb6d54b0ec6d1503f05e9e54
SisoroT <rtaylor6410@gmail.com>
2023-12-31 13:14:41 -0500

    removed default configuration

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -5,25 +5,17 @@ return {
 
     config = function()
         require("nvim-autopairs").setup({
-            -- enable_check_bracket_line = true,
-
             -- change default fast_wrap
             fast_wrap = {
                 map = "<M-w>",
-                chars = { "{", "[", "(", '"', "'" },
-                pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-                offset = 0, -- Offset from pattern match
                 end_key = "w",
-                keys = "qwertyuiopzxcvbnmasdfghjkl",
-                check_comma = true,
-                highlight = "Search",
-                hightlight_grey = "LineNr",
+                keys = "qertyuiopzxcvbnmasdfghjkl",
             },
         })
 
         -- If you want insert `(` after select function or method item
         local cmp_autopairs = require("nvim-autopairs.completion.cmp")
         local cmp = require("cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
 }
