2b7d6581de4f3c054b85ca3e1914931e46fd00d5
SisoroT <rtaylor6410@gmail.com>
2023-12-30 21:29:37 -0500

    added leap.nvim for better movement since hop is deprecated

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,55 +1,64 @@
 -- plugins w/o configs
 return {
-	-- see all diagnostics in buffer at once
-	{
-		"folke/trouble.nvim",
-		keys = { { "<leader>t", "<cmd>TroubleToggle<cr>" } },
-	},
+    -- see all diagnostics in buffer at once
+    {
+        "folke/trouble.nvim",
+        keys = { { "<leader>t", "<cmd>TroubleToggle<cr>" } },
+    },
 
-	-- lsp signatures
-	{
-		"ray-x/lsp_signature.nvim",
-		config = true,
-	},
+    -- lsp signatures
+    {
+        "ray-x/lsp_signature.nvim",
+        config = true,
+    },
 
-	-- better comments (adds TODO, FIXME, etc)
-	{
-		"folke/todo-comments.nvim",
-		config = true,
-	},
+    -- better comments (adds TODO, FIXME, etc)
+    {
+        "folke/todo-comments.nvim",
+        config = true,
+    },
 
-	-- visualize git conflicts
-	{ "akinsho/git-conflict.nvim", version = "*", config = true },
+    -- visualize git conflicts
+    { "akinsho/git-conflict.nvim",           version = "*", config = true },
 
-	-- sudo save
-	{
-		"lambdalisue/suda.vim",
-		init = vim.api.nvim_create_user_command("W", "SudaWrite", {}),
-	},
+    -- sudo save
+    {
+        "lambdalisue/suda.vim",
+        init = vim.api.nvim_create_user_command("W", "SudaWrite", {}),
+    },
 
-	-- kill bad habits :)
-	{
-		"m4xshen/hardtime.nvim",
-		config = true,
-	},
+    -- kill bad habits :)
+    {
+        "m4xshen/hardtime.nvim",
+        config = true,
+    },
 
-	-- blankline for tabs
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    -- better movement
+    {
+        "ggandor/leap.nvim",
+        keys = { "s", "S" },
+        config = function()
+            require("leap").create_default_mappings()
+        end,
+    },
 
-	-- highlights colors behind hex, rgb, and names
-	{
-		"NvChad/nvim-colorizer.lua",
-		config = true,
-	},
+    -- blankline for tabs
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {} },
 
-	-- markdown preview
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = "markdown",
-		keys = { { "<leader>mp", ":MarkdownPreviewToggle<cr>" } },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+    -- highlights colors behind hex, rgb, and names
+    {
+        "NvChad/nvim-colorizer.lua",
+        config = true,
+    },
+
+    -- markdown preview
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = "markdown",
+        keys = { { "<leader>mp", ":MarkdownPreviewToggle<cr>" } },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
