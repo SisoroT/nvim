3b7470e4e29635b422813a5990940569a8c368db
SisoroT <rtaylor6410@gmail.com>
2023-12-31 13:13:44 -0500

    added csharpier formatter, forcing stylua to use spaces to fix incosistency in lua files

--- lua/plugins/lsp/null-ls-config.lua
+++ lua/plugins/lsp/null-ls-config.lua
@@ -1,68 +1,73 @@
 -- diagnostics, linting, and autoformatting
 return {
-	"nvimtools/none-ls.nvim",
+    "nvimtools/none-ls.nvim",
 
-	config = function()
-		local null_ls = require("null-ls")
-		-- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
-		local formatting = null_ls.builtins.formatting
-		local diagnostics = null_ls.builtins.diagnostics
-		local code_actions = null_ls.builtins.code_actions
+    config = function()
+        local null_ls = require("null-ls")
+        -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+        local formatting = null_ls.builtins.formatting
+        local diagnostics = null_ls.builtins.diagnostics
+        local code_actions = null_ls.builtins.code_actions
 
-		require("null-ls").setup({
-			sources = {
-				-- python
-				formatting.black,
-				diagnostics.flake8,
+        require("null-ls").setup({
+            sources = {
+                -- python
+                formatting.black,
+                diagnostics.flake8,
 
-				-- lua
-				formatting.stylua,
+                -- lua
+                formatting.stylua.with({
+                    extra_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                }),
 
-				-- javascript, css, html
-				formatting.prettier.with({
-					extra_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
-				}),
-				-- html only
-				diagnostics.tidy,
-				-- javascript only
-				diagnostics.eslint,
-				code_actions.eslint,
+                -- javascript, css, html
+                formatting.prettier.with({
+                    extra_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
+                }),
+                -- html only
+                diagnostics.tidy,
+                -- javascript only
+                diagnostics.eslint,
+                code_actions.eslint,
 
-				-- php
-				formatting.phpcbf.with({
-					extra_args = { "--standard=PSR12" },
-				}),
-				diagnostics.phpcs.with({
-					extra_args = { "--standard=PSR12", "--ignore=PEAR.Commenting.FileComment.Missing" },
-				}),
+                -- c#
+                formatting.csharpier,
 
-				-- sql
-				formatting.sqlfluff.with({
-					extra_args = { "--dialect", "mysql" },
-				}),
-				diagnostics.sqlfluff.with({
-					extra_args = { "--dialect", "mysql", "--exclude-rules", "L044" },
-				}),
+                -- php
+                formatting.phpcbf.with({
+                    extra_args = { "--standard=PSR12" },
+                }),
+                diagnostics.phpcs.with({
+                    extra_args = { "--standard=PSR12", "--ignore=PEAR.Commenting.FileComment.Missing" },
+                }),
 
-				-- shell
-				formatting.shfmt,
-				diagnostics.shellcheck,
+                -- sql
+                formatting.sqlfluff.with({
+                    extra_args = { "--dialect", "mysql" },
+                }),
+                diagnostics.sqlfluff.with({
+                    extra_args = { "--dialect", "mysql", "--exclude-rules", "L044" },
+                }),
 
-				-- spellchecking
-				diagnostics.codespell,
-			},
-			on_attach = function(client, bufnr)
-				if client.server_capabilities.documentFormattingProvider then
-					local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = group,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
-		})
-	end,
+                -- shell
+                formatting.shfmt,
+                diagnostics.shellcheck,
+
+                -- spellchecking
+                diagnostics.codespell,
+            },
+            on_attach = function(client, bufnr)
+                if client.server_capabilities.documentFormattingProvider then
+                    local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = group,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
+        })
+    end,
 }
