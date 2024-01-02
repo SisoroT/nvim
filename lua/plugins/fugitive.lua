550568b933f77b55560c57e319efe347f0d321d4
SisoroT <rtaylor6410@gmail.com>
2024-01-01 21:37:20 -0500

    added keybind to toggle vim fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,24 @@
 return {
     "tpope/vim-fugitive",
     event = "BufWinEnter",
+    config = function()
+        local function showFugitiveGit()
+            if vim.fn.FugitiveHead() ~= "" then
+                vim.cmd([[
+                Git
+                resize 20
+                ]])
+            end
+        end
+
+        local function toggleFugitiveGit()
+            if vim.fn.buflisted(vim.fn.bufname("fugitive:///*/.git//$")) ~= 0 then
+                vim.cmd([[ execute ':bdelete' bufname('fugitive:///*/.git//$') ]])
+            else
+                showFugitiveGit()
+            end
+        end
+
+        vim.keymap.set("n", "<leader>g", toggleFugitiveGit)
+    end,
 }
