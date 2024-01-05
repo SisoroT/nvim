21aa7d53ed75230d3c4dc4fa7cdc0e6ec36037e6
SisoroT <rtaylor6410@gmail.com>
2024-01-05 18:37:33 -0500

    switched from nvim tree to oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+    "stevearc/oil.nvim",
+    keys = {
+        { "-", "<CMD>Oil<CR>" },
+    },
+    opts = {
+        view_options = {
+            show_hidden = true,
+        },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+}
