9e5b9af73507441834b37af770e6e4af40b23835
SisoroT <rtaylor6410@gmail.com>
2024-01-13 18:29:07 -0500

    renamed cmp file and a bit of refactoring

--- lua/plugins/autocomplete/cmp.lua
+++ lua/plugins/autocomplete/cmp.lua
@@ -8,42 +8,21 @@ return {
         -- snippets
         {
             "L3MON4D3/LuaSnip",
-            event = "InsertEnter",
             dependencies = { "rafamadriz/friendly-snippets" },
-            keys = {
-                -- jump forwards
-                {
-                    "<C-j>",
-                    function()
-                        require("luasnip").jump(1)
-                    end,
-                    mode = { "i", "s" },
-                },
-                -- jump backwards
-                {
-                    "<C-k>",
-                    function()
-                        require("luasnip").jump(-1)
-                    end,
-                    mode = { "i", "s" },
-                },
-            },
-            config = function()
-                require("luasnip.loaders.from_vscode").lazy_load()
-            end,
         },
         -- autocomplete sources
-        { "hrsh7th/cmp-nvim-lua", event = "InsertEnter" },
-        { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
-        { "hrsh7th/cmp-buffer", event = "InsertEnter" },
-        { "hrsh7th/cmp-path", event = "InsertEnter" },
-        { "hrsh7th/cmp-calc", event = "InsertEnter" },
-        { "saadparwaiz1/cmp_luasnip", event = "InsertEnter" },
+        { "hrsh7th/cmp-nvim-lua" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-path" },
+        { "hrsh7th/cmp-calc" },
+        { "saadparwaiz1/cmp_luasnip" },
     },
 
     config = function()
         local cmp = require("cmp")
         local lspkind = require("lspkind")
+        local ls = require("luasnip")
 
         cmp.setup({
             completion = {
@@ -56,13 +35,12 @@ return {
             },
             mapping = {
                 ["<C-Space>"] = cmp.mapping.complete(),
-                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                ["<C-e>"] = cmp.mapping.close(),
                 ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<M-e>"] = cmp.mapping.close(),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Insert,
                     select = true,
                 }),
             },
@@ -71,7 +49,6 @@ return {
                     mode = "symbol", -- show only symbol annotations
                 }),
             },
-            -- You should specify your *installed* sources.
             sources = {
                 { name = "luasnip" },
                 { name = "nvim_lsp" },
@@ -81,5 +58,16 @@ return {
                 { name = "calc" },
             },
         })
+
+        -- LuaSnip
+        require("luasnip.loaders.from_vscode").lazy_load()
+        -- jump forwards
+        vim.keymap.set({ "i", "s" }, "<C-j>", function()
+            ls.jump(1)
+        end)
+        -- jump backwards
+        vim.keymap.set({ "i", "s" }, "<C-k>", function()
+            ls.jump(-1)
+        end)
     end,
 }
