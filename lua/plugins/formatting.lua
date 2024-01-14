bea79c14a07cbb4a1a756f482f374882000e9951
SisoroT <rtaylor6410@gmail.com>
2024-01-14 10:22:02 -0500

    figured out how to add extra args for stylua and prettier

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -5,25 +5,33 @@ return {
     config = function()
         require("conform").setup({
             formatters_by_ft = {
-                -- lua = { "stylua" },
+                lua = { "stylua" },
                 python = { "black" },
                 cs = { "csharpier" },
-                bash = { "shfmt" },
+                sh = { "shfmt" },
 
                 -- prettier
-                -- javascript = { "prettier" },
-                -- typescript = { "prettier" },
-                -- javascriptreact = { "prettier" },
-                -- typescriptreact = { "prettier" },
-                -- css = { "prettier" },
-                -- html = { "prettier" },
-                -- json = { "prettier" },
-                -- markdown = { "prettier" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                markdown = { "prettier" },
             },
             format_on_save = {
                 lsp_fallback = true,
                 async = false,
                 timeout_ms = 500,
+            },
+            formatters = {
+                stylua = {
+                    prepend_args = { "--indent-type", "Spaces" },
+                },
+                prettier = {
+                    prepend_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
+                },
             },
         })
     end,
