3aeb9fc36430fcdf7fac5f91868287d797a5d931
SisoroT <rtaylor6410@gmail.com>
2024-01-14 13:51:57 -0500

    added function to check if a linter is installed so typos doesn't break everything

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -5,6 +5,14 @@ return {
     config = function()
         local lint = require("lint")
 
+        local function is_linter_installed(linter)
+            if vim.fn.executable(linter) ~= 1 then
+                vim.notify(linter .. " is not installed.", vim.log.levels.WARN)
+                return false
+            end
+            return true
+        end
+
         lint.linters_by_ft = {
             python = { "flake8" },
             javascript = { "eslint_d" },
@@ -19,7 +27,10 @@ return {
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             callback = function()
                 lint.try_lint()
-                lint.try_lint("typos")
+
+                if is_linter_installed("typos") then
+                    lint.try_lint("typos")
+                end
             end,
         })
     end,
