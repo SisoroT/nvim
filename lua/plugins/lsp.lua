96daf01622cc50912e88615eaf7c4ebd770c8498
SisoroT <rtaylor6410@gmail.com>
2025-09-17 15:16:13 -0400

    update name of ts languauge server in mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -61,7 +61,7 @@ return {
                 "lua_ls",
                 "omnisharp",
                 "pyright",
-                "tsserver",
+                "ts_ls",
                 "vimls",
             },
 
