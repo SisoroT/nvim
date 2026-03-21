31750249abdcece551e7f1a7e4b3193ab31b6b78
SisoroT <rtaylor6410@gmail.com>
2026-03-21 15:14:11 -0400

    refactor(lsp): modernize and improve lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,112 +1,85 @@
 return {
     "neovim/nvim-lspconfig",
-    build = ":MasonUpdate",
-
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "Hoffs/omnisharp-extended-lsp.nvim",
+        {
+            "mason-org/mason.nvim",
+            opts = {
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            },
+        },
+        "mason-org/mason-lspconfig.nvim",
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
     },
 
     config = function()
-        local lspconfig = require("lspconfig")
-
-        -- needed for cssls
+        -- needed for css autocomplete
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-        -- change directory to root of project
-        local on_attach = function(client)
-            local root_dir = client.config.root_dir
-            vim.cmd("cd " .. root_dir)
-        end
+        local servers = {
+            "bashls",
+            "cssls",
+            "html",
+            "jsonls",
+            "lua_ls",
+            "pyright",
+            "ts_ls",
+            "vimls",
+        }
 
-        -- keybinds
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover)
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
-        vim.keymap.set("n", "H", vim.lsp.buf.signature_help)
-        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-        vim.keymap.set("n", "H", vim.diagnostic.open_float)
-        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder)
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder)
-        vim.keymap.set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end)
-
-        -- icons for mason
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
-
-        -- ensure language servers and setup handlers
+        -- Ensure servers are installed
         require("mason-lspconfig").setup({
-            ensure_installed = {
-                "bashls",
-                "cssls",
-                "html",
-                "jsonls",
-                "lua_ls",
-                "omnisharp",
-                "pyright",
-                "ts_ls",
-                "vimls",
-            },
-
-            handlers = {
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        on_attach = on_attach,
-                    })
-                end,
-
-                ["lua_ls"] = function()
-                    lspconfig.lua_ls.setup({
-                        on_attach = on_attach,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                            },
-                        },
-                    })
-                end,
-                ["cssls"] = function()
-                    lspconfig.cssls.setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-                ["omnisharp"] = function()
-                    lspconfig.omnisharp.setup({
-                        on_attach = on_attach,
-                        handlers = {
-                            ["textDocument/definition"] = require("omnisharp_extended").handler,
-                        },
-                        cmd = { "omnisharp", "--languageserver" },
-                    })
-                end,
-            },
+            ensure_installed = servers,
         })
 
-        -- open mason
-        vim.keymap.set("n", "<leader>m", ":Mason<CR>")
-        -- toggle diagnostic
+        -- Apply capabilities to all servers
+        vim.lsp.config("*", {
+            capabilities = capabilities,
+        })
+        -- Enable all servers using native API
+        vim.lsp.enable(servers)
+
+        -- Buffer-local LSP keymaps
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+            callback = function(ev)
+                local opts = { buffer = ev.buf, silent = true }
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "H", vim.diagnostic.open_float, opts)
+
+                -- cd to root on attach
+                local client = vim.lsp.get_client_by_id(ev.data.client_id)
+                if client and client.config.root_dir then
+                    vim.cmd("lcd " .. vim.fn.fnameescape(client.workspace_folders[1].name))
+                end
+            end,
+        })
+
+        -- Global keymaps
+        vim.keymap.set("n", "<leader>m", "<cmd>Mason<CR>", { silent = true })
         vim.keymap.set("n", "<leader>dd", function()
             vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-        end, { silent = true, noremap = true })
+        end, { silent = true })
     end,
 }
