5df8e1dcd8dbabdec584e05021d656f667a637b1
SisoroT <rtaylor6410@gmail.com>
2026-03-21 18:27:02 -0400

    fix(formatting): increase timeout and use new api value for fallback

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,6 +1,7 @@
 return {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    cmd = "ConformInfo",
 
     config = function()
         require("conform").setup({
@@ -21,9 +22,8 @@ return {
                 yaml = { "prettier" },
             },
             format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
+                lsp_format = "fallback",
+                timeout_ms = 1000,
             },
             formatters = {
                 stylua = {
