d3bf65677ac5ff9447ac22ce4cff75af3269313a
SisoroT <rtaylor6410@gmail.com>
2026-03-21 18:46:33 -0400

    refactor: use built-in opts from lazy

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,36 +3,34 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     cmd = "ConformInfo",
 
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "black" },
-                sh = { "shfmt" },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "black" },
+            sh = { "shfmt" },
 
-                -- prettier
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                markdown = { "prettier" },
-                yaml = { "prettier" },
+            -- prettier
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            css = { "prettier" },
+            html = { "prettier" },
+            json = { "prettier" },
+            markdown = { "prettier" },
+            yaml = { "prettier" },
+        },
+        format_on_save = {
+            lsp_format = "fallback",
+            timeout_ms = 1000,
+        },
+        formatters = {
+            stylua = {
+                prepend_args = { "--indent-type", "Spaces" },
             },
-            format_on_save = {
-                lsp_format = "fallback",
-                timeout_ms = 1000,
+            prettier = {
+                prepend_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
             },
-            formatters = {
-                stylua = {
-                    prepend_args = { "--indent-type", "Spaces" },
-                },
-                prettier = {
-                    prepend_args = { "--tab-width", "4", "--no-semi", "--single-quote", "--jsx-single-quote" },
-                },
-            },
-        })
-    end,
+        },
+    },
 }
