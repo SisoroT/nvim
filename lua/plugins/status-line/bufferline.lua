766bb64b5436bf0ee16da588b9a612b14a4818df
SisoroT <rtaylor6410@gmail.com>
2026-03-21 18:49:47 -0400

    refactor: use built-in opts from lazy

--- lua/plugins/status-line/bufferline.lua
+++ lua/plugins/status-line/bufferline.lua
@@ -2,21 +2,18 @@
 return {
     "akinsho/bufferline.nvim",
     event = "BufEnter",
-
-    config = function()
-        require("bufferline").setup({
-            options = {
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "NvimTree",
-                        highlight = "Directory",
-                        text_align = "left",
-                    },
+    opts = {
+        options = {
+            show_buffer_close_icons = false,
+            show_close_icon = false,
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "NvimTree",
+                    highlight = "Directory",
+                    text_align = "left",
                 },
             },
-        })
-    end,
+        },
+    },
 }
