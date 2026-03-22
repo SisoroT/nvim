6b67adc16e2e9cdb64310429b4d917015a703d08
SisoroT <rtaylor6410@gmail.com>
2026-03-22 14:47:59 -0400

    refactor: update telescope and add some new binds for finding buffers, recent files, etc

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,34 +1,59 @@
 -- fuzzy file finder
 return {
     "nvim-telescope/telescope.nvim",
-    module = false,
-    keys = {
-        { "<leader>ff" },
-        { "<leader>fc" },
-        { "<leader>fg" },
-        { "<leader>fw" },
-        { "<leader>fW" },
-        { "<leader>vh" },
-        { "<leader>vk" },
-        { "gr" },
-    },
     dependencies = {
         "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+
+    cmd = { "Telescope" },
+    keys = {
+        -- file search
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+        {
+            "<leader>fc",
+            function()
+                require("telescope.builtin").find_files({
+                    prompt_title = "Search ~/.config/",
+                    cwd = vim.fn.expand("~/.config/"),
+                })
+            end,
+            desc = "Search Config",
+        },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+        { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
+        -- text search
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+        {
+            "<leader>fw",
+            function()
+                require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") })
+            end,
+            desc = "Grep Word",
+        },
+        {
+            "<leader>fW",
+            function()
+                require("telescope.builtin").grep_string({ search = vim.fn.expand("<cWORD>") })
+            end,
+            desc = "Grep WORD",
+        },
+        { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+        -- vim search
+        { "<leader>vh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+        { "<leader>vk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+        { "gr", "<cmd>Telescope lsp_references<cr>", desc = "LSP References" },
     },
 
     config = function()
         local actions = require("telescope.actions")
-        local builtin = require("telescope.builtin")
 
         require("telescope").setup({
             defaults = {
-                color_devicons = true,
                 mappings = {
                     i = {
                         ["<C-j>"] = actions.move_selection_next,
                         ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-s>"] = actions.select_horizontal,
                     },
                 },
                 file_ignore_patterns = {
@@ -37,51 +62,22 @@ return {
                 },
                 vimgrep_arguments = {
                     "rg",
-                    "--hidden",
-                    "--smart-case",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
                     "--column",
+                    "--smart-case",
+                    "--hidden",
                 },
             },
             pickers = {
                 find_files = {
-                    find_command = { "rg", "--files", "--hidden" },
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
                 },
-                live_grep = { "rg", "--hidden", "" },
             },
-            extensions = {},
         })
 
-        -- search files in current directory
-        vim.keymap.set("n", "<leader>ff", builtin.find_files)
-        -- search all files in .config folder
-        vim.keymap.set("n", "<leader>fc", function()
-            builtin.find_files({ prompt_title = "Search ~/.config/", cwd = vim.fn.expand("~/.config/") })
-        end)
-
-        -- rg strings in cwd
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-
-        -- search vim help docs
-        vim.keymap.set("n", "<leader>vh", builtin.help_tags)
-        -- search vim keymaps
-        vim.keymap.set("n", "<leader>vk", builtin.keymaps)
-
-        -- search lsp_references
-        vim.keymap.set("n", "gr", builtin.lsp_references)
-
-        -- search for word under cursor
-        vim.keymap.set("n", "<leader>fw", function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-
-        -- search for WORD under cursor
-        vim.keymap.set("n", "<leader>fW", function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-
-        -- load extensions
         require("telescope").load_extension("fzf")
     end,
 }
