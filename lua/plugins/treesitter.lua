65461d62445ee558d1d913623e47ec9f0eb3d041
SisoroT <rtaylor6410@gmail.com>
2026-03-22 14:04:49 -0400

    refactor(treesitter): transition to main branch and update config to match new apis

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,64 +1,89 @@
 -- better syntax highlighting
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     lazy = false,
     build = ":TSUpdate",
     dependencies = {
         -- rainbow parens
         "HiPhish/rainbow-delimiters.nvim",
         -- auto match html/jsx tags
-        "windwp/nvim-ts-autotag",
+        { "windwp/nvim-ts-autotag", opts = {} },
         -- show context of current buffer contents
         "nvim-treesitter/nvim-treesitter-context",
         -- operations on inner and outer text objects
-        "nvim-treesitter/nvim-treesitter-textobjects",
+        { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
     },
 
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all" (the listed parsers should always be installed)
-            ensure_installed = {
-                "bash",
-                "c",
-                "comment",
-                "css",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "python",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-            },
-            highlight = {
-                enable = true,
-            },
-            indent = {
-                enable = false,
-            },
-            autotag = {
-                enable = true,
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
-                    },
-                },
-            },
+        require("nvim-treesitter").install({
+            "bash",
+            "c",
+            "comment",
+            "css",
+            "html",
+            "javascript",
+            "jsdoc",
+            "json",
+            "jsonc",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "regex",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "yaml",
         })
+
+        -- enable treesitter based syntax highlighting
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+            end,
+        })
+
+        -- Text Object Binds
+        local tobj_select = require("nvim-treesitter-textobjects.select").select_textobject
+        -- functions
+        vim.keymap.set({ "x", "o" }, "af", function()
+            tobj_select("@function.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "if", function()
+            tobj_select("@function.inner", "textobjects")
+        end)
+        -- parameters/arguments
+        vim.keymap.set({ "x", "o" }, "aa", function()
+            tobj_select("@parameter.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "ia", function()
+            tobj_select("@parameter.inner", "textobjects")
+        end)
+        -- conditionals
+        vim.keymap.set({ "x", "o" }, "ai", function()
+            tobj_select("@conditional.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "ii", function()
+            tobj_select("@conditional.inner", "textobjects")
+        end)
+        -- loops
+        vim.keymap.set({ "x", "o" }, "al", function()
+            tobj_select("@loop.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "il", function()
+            tobj_select("@loop.inner", "textobjects")
+        end)
+        -- classes
+        vim.keymap.set({ "x", "o" }, "ac", function()
+            tobj_select("@class.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "ic", function()
+            tobj_select("@class.inner", "textobjects")
+        end)
     end,
 }
