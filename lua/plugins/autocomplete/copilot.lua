56b60f88ba421f79c54076aec52b60f4269b21c0
SisoroT <rtaylor6410@gmail.com>
2026-04-10 13:21:35 -0400

    refactor: add codecomanion plugin, change nes bind to Tab; remove old copilot binds since blink handles now

--- lua/plugins/autocomplete/copilot.lua
+++ lua/plugins/autocomplete/copilot.lua
@@ -1,27 +1,23 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-
+    "olimorris/codecompanion.nvim",
+    version = "^19.0.0",
+    opts = {},
     dependencies = {
-        "copilotlsp-nvim/copilot-lsp",
-    },
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        {
+            "zbirenbaum/copilot.lua",
+            dependencies = { "copilotlsp-nvim/copilot-lsp" },
 
-    opts = {
-        suggestion = {
-            auto_trigger = true,
-            keymap = {
-                accept = "<M-n>",
-                prev = "<M-e>",
-                next = "<M-i>",
-                dismiss = "<M-o>",
-            },
-        },
-        nes = {
-            enabled = true,
-            keymap = {
-                accept_and_goto = "<M-h>",
-                dismiss = "<Esc>",
+            opts = {
+                suggestion = { enabled = false }, -- blink-copilot handles suggestions
+                nes = {
+                    enabled = true,
+                    keymap = {
+                        accept_and_goto = "<Tab>",
+                        dismiss = "<Esc>",
+                    },
+                },
             },
         },
     },
