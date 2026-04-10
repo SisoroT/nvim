8788c128851154155454611f7c306c08a9d88ce3
SisoroT <rtaylor6410@gmail.com>
2026-04-10 13:18:34 -0400

    feat: add copilot source to blink suggestion menu

--- lua/plugins/autocomplete/cmp.lua
+++ lua/plugins/autocomplete/cmp.lua
@@ -1,6 +1,6 @@
 return {
     "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = { "rafamadriz/friendly-snippets", "fang2hou/blink-copilot" },
     version = "1.*",
 
     opts = {
@@ -10,16 +10,23 @@ return {
             ["<C-u>"] = { "scroll_documentation_up", "fallback" },
         },
         sources = {
-            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            default = { "lazydev", "lsp", "path", "snippets", "buffer", "copilot" },
             providers = {
                 lazydev = {
                     name = "LazyDev",
                     module = "lazydev.integrations.blink",
-                    score_offset = 100, -- show at a higher priority than lsp
+                    score_offset = 100,
+                },
+                copilot = {
+                    name = "copilot",
+                    module = "blink-copilot",
+                    score_offset = 100,
+                    async = true,
                 },
             },
         },
         completion = {
+            ghost_text = { enabled = true },
             menu = { border = "rounded" },
             documentation = {
                 auto_show = true,
